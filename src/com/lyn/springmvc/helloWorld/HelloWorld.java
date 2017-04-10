4c412c3c6e9b44bbe0fa1d6c93be093987f87b08
BlackLee_home <webitlee@163.com>
2017-04-10 23:35:08 +0800

    添加了springMVC的helloWorld的demo

---
+++ src/com/lyn/springmvc/helloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.lyn.springmvc.helloWorld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorld {
+	//使用RequestMapping来映射请求的url
+	//返回值通过视图解析器解析为实际的物理视图，对于InternalResourceViewResolver视图解析器，会做如下解析：
+	//通过prefix + returnValue + stuffix的方式得到实际的物理视图，然后做转发操作
+	// /WEB-INF/views/helloWorld.jsp
+	@RequestMapping("/hello")
+	public String hello(){
+		System.out.println("HELLO WORLD");
+		return "helloWorld";
+	}
+}
