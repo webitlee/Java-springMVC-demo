7824b87724a71b2003271e7fed0d6bd421c7f598
BlackLee_home <webitlee@163.com>
2017-04-25 21:30:00 +0800

    修改了文件上传的demo，尝试进行图片上传保存操作，基本成功

--- src/com/lyn/springmvc/fileUpload/TestFileUpload.java
+++ src/com/lyn/springmvc/fileUpload/TestFileUpload.java
@@ -1,6 +1,10 @@
 package com.lyn.springmvc.fileUpload;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +15,13 @@ import org.springframework.web.multipart.MultipartFile;
 public class TestFileUpload {
 
 	@RequestMapping("/testFileUpload")
-	public String testFileUpload(@RequestParam("desc") String desc, @RequestParam("file") MultipartFile file) throws IOException{
+	public String testFileUpload(@RequestParam("desc") String desc, @RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException{
 		System.out.println(desc);
 		System.out.println(file.getOriginalFilename());
 		System.out.println(file.getInputStream());
+		 // 项目在容器中实际发布运行的根路径
+		//File coyp = new File(request.getSession().getServletContext().getRealPath("/") + String.valueOf(System.currentTimeMillis()) + file.getOriginalFilename());
+		//file.transferTo(coyp);
 		return "modelDataTest";
 	}
 }
